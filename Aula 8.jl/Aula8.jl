dc9b62c78be3bb3c9a2698f47e5dda5431356eab
Gustavo Rios <111003046+gustavorioshonorato@users.noreply.github.com>
2022-09-24 16:05:47 +0000

    To morto de cansado professor

---
+++ Aula 8.jl/Aula8.jl
@@ -0,0 +1,25 @@
+using LinearAlgebra
+
+function fixedpoint(a,g, error)
+x=g(a)
+while norm(x-a) > error
+a=x
+x=g(a)
+end
+return x
+end
+
+F(x) = [((cos(x1) + 2x[1] - x[2]) ;
+      ((x[1]^2) + (x[2]^2)/4 - 1)]
+
+J(x) = [(2*x[1]/3) (2*x[2])
+        (2*x[1]) (x[2]/2)]
+
+g(x) = x - (inv(J(x))*F(x))
+
+erro = 10^-8
+
+x = [1 ; 1]
+
+r = fixedpoint(x, g, erro)
+println(r)
